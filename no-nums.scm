b661428cedd0cf0427c7d6632862e708b09e2087
halfalpine <rocksandwoks@gmail.com>
2016-09-01 10:27:22 -0400

    no-nums.scm

---
+++ no-nums.scm
@@ -0,0 +1,7 @@
+(define no-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) (quote()))
+      (else (cond
+        ((number? (car lat)) (no-nums (cdr lat))
+        (else (cons (car lat) (no-nums (cdr lat))))))))))

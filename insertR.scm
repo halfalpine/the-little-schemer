09416019de11463accfb8700370c443c3df265df
halfalpine <rocksandwoks@gmail.com>
2016-09-05 11:04:59 -0400

    Add insertR* to insertR.scm

--- insertR.scm
+++ insertR.scm
@@ -17,5 +17,15 @@ and
 
 function insertR (new, old, arr) {
   if (arr.length = 0) return null;
-  
+
 }
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+      ((null? l) (quote()))
+      ((atom? (car l))
+        (cond
+          ((eq? (car l) old) (cons old (cons new (insertR* (new old (cdr l)))))
+          (else (cons car l) (insertR* (new old (cdr l))))))
+      (else (cons (insertR* new old (car l)) (insertR* (new old (cdr l)))))))))

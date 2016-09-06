f3c5852cd8461a6744caa0d8bdf022c120d78cca
halfalpine <rocksandwoks@gmail.com>
2016-09-06 10:46:03 -0400

    Add insertL* to insertL.scm

--- insertL.scm
+++ insertL.scm
@@ -7,3 +7,12 @@
       (else (cond
         ((eq? (car lat) old) (cons new (cons old (cdr lat)))))
         (else (cons (car lat) (insertL new old (cdr lat)))))))))
+
+(define insertL*
+  (lambda (new old l)
+    (cond
+      ((null? l) (quote()))
+      ((atom? (car l))
+        ((eq? (car l) old) (cons new (cons old (insertL* (cdr l)))))
+        (else (cons (car l) insertL* new old (cdr l))))))
+      (else (cons (insertL* new old (car l)) (insertL* new old (cdr l)))))

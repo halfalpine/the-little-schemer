e42f9806bd0f327cecb30ec58bb96192d5279b27
halfalpine <rocksandwoks@gmail.com>
2016-09-06 10:37:12 -0400

    Add subst* to subst.scm

--- subst.scm
+++ subst.scm
@@ -5,3 +5,13 @@
       (else (cond
         ((eq? (car lat) old) (cons new (cdr lat)))))))
         (else (cons (car lat) (subst new old (cdr lat))))))
+
+(define subst*
+  (lambda (new old l)
+    (cond
+      ((null? l) (quote()))
+      ((atom? (car l))
+        (cond
+          ((eq? (car l) old) (cons new (subst* new old (cdr l)))))
+          (else (cons (car l) (subst* new old (cdr l))))
+      (else (cons subst* new old (car l) subst* new old (cdr l))))))))
